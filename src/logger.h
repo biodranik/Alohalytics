6a408533303a18e3c66365df3ef422744900b796
Alex Zolotarev <alex@maps.me>
2014-12-13 23:50:40 -1000

    Pretty logging for std containers

--- src/logger.h
+++ src/logger.h
@@ -1,24 +1,31 @@
 #include <sstream>
 
 #if defined(__OBJC__)
-  #include <Foundation/Foundation.h>
+#include <Foundation/Foundation.h>
 #elif defined(ANDROID)
-  #include <android/log.h>
+#include <android/log.h>
 #else
-  #include <iostream>
+#include <iostream>
 #endif
+
+// Pretty-printing for std::pair.
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& out, const std::pair<T, U>& p) {
+  out << "[" << p.first << "=" << p.second << "]";
+  return out;
+}
 
 namespace aloha {
 
 class Logger {
   std::ostringstream out_;
 
-public:
+ public:
   Logger() {
   }
 
-  Logger(const char * file, int line) {
-    out_ << file << ":" << line << ": ";
+  Logger(const char* file, int line) {
+    out_ << file << ':' << line << ": ";
   }
 
   ~Logger() {
@@ -34,17 +41,11 @@ public:
 #endif
   }
 
-  template <typename T, typename ... ARGS>
-  void Log(const T& arg1, const ARGS& ... others) {
-    //Log(out_, arg1);
-    out_ << arg1 << " ";
+  template <typename T, typename... ARGS>
+  void Log(const T& arg1, const ARGS&... others) {
+    out_ << arg1 << ' ';
     Log(others...);
   }
-
-//  template<typename T>
-//  void Log(const T& t) {
-//    Log(out_, t);
-//  }
 
   void Log() {
   }
@@ -53,15 +54,30 @@ public:
   void Log(const T& t) {
     out_ << t;
   }
+
+  // String specialization to avoid printing every character as a container's element.
+  void Log(const std::string& t) {
+    out_ << t;
+  }
+
+  // Pretty-printing for containers.
+  template <template <typename, typename...> class ContainerType, typename ValueType, typename... Args>
+  void Log(const ContainerType<ValueType, Args...>& c) {
+    out_ << '{';
+    size_t index = 0;
+    const size_t count = c.size();
+    for (const auto& v : c) {
+      out_ << v << (++index == count ? '}' : ',');
+    }
+  }
 };
 
-} // namespace aloha
-
+}  // namespace aloha
 
 #if defined(__OBJC__) || defined(ANDROID)
-  #define TRACE(...) aloha::Logger(__FILE__, __LINE__ ).Log(__VA_ARGS__)
-  #define LOG(...) aloha::Logger().Log(__VA_ARGS__)
+#define TRACE(...) aloha::Logger(__FILE__, __LINE__).Log(__VA_ARGS__)
+#define LOG(...) aloha::Logger().Log(__VA_ARGS__)
 #else
-  #define TRACE(...) aloha::Logger(__FILE__, __LINE__ ).Log(__VA_ARGS__)
-  #define LOG(...) aloha::Logger().Log(__VA_ARGS__)
+#define TRACE(...) aloha::Logger(__FILE__, __LINE__).Log(__VA_ARGS__)
+#define LOG(...) aloha::Logger().Log(__VA_ARGS__)
 #endif
