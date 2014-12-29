b0700a000607309a58b1d16dc804b3c70a6c150f
Alex Zolotarev <alex@maps.me>
2014-12-29 12:38:40 -0600

    [android] Collect more statistics about the device.

--- src/android/src/main/java/org/alohastats/lib/SystemInfo.java
+++ src/android/src/main/java/org/alohastats/lib/SystemInfo.java
@@ -35,25 +35,10 @@ import android.util.Log;
 
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.HashMap;
 
-import static android.provider.Settings.Secure;
-
-interface KeyValueStorage {
-  public void set(String key, String value);
-
-  public void set(String key, float value);
-
-  public void set(String key, boolean value);
-
-  public void set(String key, int value);
-}
-
 public class SystemInfo {
-  private static final String TAG = "AlohaLytics.SystemInfo";
+  private static final String TAG = "Alohalytics.SystemInfo";
 
   private static void handleException(Exception ex) {
     if (Statistics.debugMode()) {
@@ -68,25 +53,48 @@ public class SystemInfo {
     // Collect all information on a separate thread, because:
     // - Google Advertising ID should be requested in a separate thread.
     // - Do not block UI thread while querying many properties.
-    // Collect Google Play Advertising ID
     new Thread(new Runnable() {
       @Override
       public void run() {
         collectIds(activity);
+        collectDeviceDetails(activity);
       }
     }).start();
   }
 
+  // Used for convenient null-checks.
+  private static class KeyValueWrapper {
+    public HashMap<String,String> mPairs = new HashMap<>();
+
+    public void put(String key, String value) {
+      if (key != null && value != null) {
+        mPairs.put(key, value == null ? "" : value);
+      }
+    }
+    public void put(String key, float value) {
+      if (key != null) {
+        mPairs.put(key, String.valueOf(value));
+      }
+    }
+    public void put(String key, boolean value) {
+      if (key != null) {
+        mPairs.put(key, String.valueOf(value));
+      }
+    }
+    public void put(String key, int value) {
+      if (key != null) {
+        mPairs.put(key, String.valueOf(value));
+      }
+    }
+  }
+
   private static void collectIds(final Activity activity) {
 
-    final HashMap<String,String> ids = new HashMap<>();
+    final KeyValueWrapper ids = new KeyValueWrapper();
     // Retrieve GoogleAdvertisingId.
     // See sample code at http://developer.android.com/google/play-services/id.html
     try {
-      final String google_advertising_id = AdvertisingIdClient.getAdvertisingIdInfo(activity.getApplicationContext()).getId();
-      if (google_advertising_id != null) {
-        ids.put("google_advertising_id", google_advertising_id);
-      }
+      ids.put("google_advertising_id", AdvertisingIdClient.getAdvertisingIdInfo(activity.getApplicationContext()).getId());
     } catch (Exception ex) {
       handleException(ex);
     }
@@ -94,7 +102,7 @@ public class SystemInfo {
     try {
       final String android_id = Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID);
       // This is a known bug workaround - https://code.google.com/p/android/issues/detail?id=10603
-      if (android_id != null && !android_id.equals("9774d56d682e549c")) {
+      if (!android_id.equals("9774d56d682e549c")) {
         ids.put("android_id", android_id);
       }
     } catch (Exception ex) {
@@ -104,121 +112,112 @@ public class SystemInfo {
     try {
       // This code works only if the app has READ_PHONE_STATE permission.
       final TelephonyManager tm = (TelephonyManager) activity.getBaseContext().getSystemService(activity.TELEPHONY_SERVICE);
-      final String device_id = tm.getDeviceId();
-      if (device_id != null) {
-        ids.put("device_id", device_id);
-      }
-      final String sim_serial_number = tm.getSimSerialNumber();
-      if (sim_serial_number != null) {
-        ids.put("sim_serial_number", sim_serial_number);
-      }
+      ids.put("device_id", tm.getDeviceId());
+      ids.put("sim_serial_number", tm.getSimSerialNumber());
     } catch (Exception ex) {
       handleException(ex);
     }
 
-    Statistics.logEvent("$AndroidIds", ids);
+    Statistics.logEvent("$AndroidIds", ids.mPairs);
   }
 
-  public static String emptyIfNull(final String str) {
-    return str == null ? "" : str;
-  }
+  private static void collectDeviceDetails(android.app.Activity activity) {
 
-  public void getDeviceDetails(android.app.Activity activity, KeyValueStorage kvs) {
+    final KeyValueWrapper kvs = new KeyValueWrapper();
+
     final DisplayMetrics metrics = new DisplayMetrics();
     activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-    kvs.set("display_density", metrics.density);
-    kvs.set("display_density_dpi", metrics.densityDpi);
-    kvs.set("display_scaled_density", metrics.scaledDensity);
-    kvs.set("display_width_pixels", metrics.widthPixels);
-    kvs.set("display_height_pixels", metrics.heightPixels);
-    kvs.set("display_xdpi", metrics.xdpi);
-    kvs.set("display_ydpi", metrics.ydpi);
+    kvs.put("display_density", metrics.density);
+    kvs.put("display_density_dpi", metrics.densityDpi);
+    kvs.put("display_scaled_density", metrics.scaledDensity);
+    kvs.put("display_width_pixels", metrics.widthPixels);
+    kvs.put("display_height_pixels", metrics.heightPixels);
+    kvs.put("display_xdpi", metrics.xdpi);
+    kvs.put("display_ydpi", metrics.ydpi);
 
     final Configuration config = activity.getResources().getConfiguration();
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-      kvs.set("dpi", config.densityDpi); // int value
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+      kvs.put("dpi", config.densityDpi); // int value
+    }
 
-    kvs.set("font_scale", config.fontScale);
-    kvs.set("locale_country", config.locale.getCountry());
-    kvs.set("locale_language", config.locale.getLanguage());
-    kvs.set("locale_variant", config.locale.getVariant());
+    kvs.put("font_scale", config.fontScale);
+    kvs.put("locale_country", config.locale.getCountry());
+    kvs.put("locale_language", config.locale.getLanguage());
+    kvs.put("locale_variant", config.locale.getVariant());
 
-    if (config.mcc != 0)
-      kvs.set("mcc", config.mcc);
-    if (config.mnc != 0)
-      kvs.set("mnc", config.mnc == Configuration.MNC_ZERO ? 0 : config.mnc);
+    kvs.put("mcc", config.mcc);
+    kvs.put("mnc", config.mnc == Configuration.MNC_ZERO ? 0 : config.mnc);
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-      kvs.set("screen_width_dp", config.screenWidthDp);
-      kvs.set("screen_height_dp", config.screenHeightDp);
+      kvs.put("screen_width_dp", config.screenWidthDp);
+      kvs.put("screen_height_dp", config.screenHeightDp);
     }
 
     final ContentResolver cr = activity.getContentResolver();
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-      kvs.set(Settings.Global.AIRPLANE_MODE_ON, Settings.Global.getString(cr, Settings.Global.AIRPLANE_MODE_ON)); // 1 or 0
-      kvs.set(Settings.Global.ALWAYS_FINISH_ACTIVITIES, Settings.Global.getString(cr, Settings.Global.ALWAYS_FINISH_ACTIVITIES)); // 1 or 0
-      kvs.set(Settings.Global.AUTO_TIME, Settings.Global.getString(cr, Settings.Global.AUTO_TIME)); // 1 or 0
-      kvs.set(Settings.Global.AUTO_TIME_ZONE, Settings.Global.getString(cr, Settings.Global.AUTO_TIME_ZONE)); // 1 or 0
-      kvs.set(Settings.Global.BLUETOOTH_ON, Settings.Global.getString(cr, Settings.Global.BLUETOOTH_ON)); // 1 or 0
-      kvs.set(Settings.Global.DATA_ROAMING, Settings.Global.getString(cr, Settings.Global.DATA_ROAMING));  // 1 or 0
-      kvs.set(Settings.Global.HTTP_PROXY, Settings.Global.getString(cr, Settings.Global.HTTP_PROXY));  // host:port
+      kvs.put(Settings.Global.AIRPLANE_MODE_ON, Settings.Global.getString(cr, Settings.Global.AIRPLANE_MODE_ON)); // 1 or 0
+      kvs.put(Settings.Global.ALWAYS_FINISH_ACTIVITIES, Settings.Global.getString(cr, Settings.Global.ALWAYS_FINISH_ACTIVITIES)); // 1 or 0
+      kvs.put(Settings.Global.AUTO_TIME, Settings.Global.getString(cr, Settings.Global.AUTO_TIME)); // 1 or 0
+      kvs.put(Settings.Global.AUTO_TIME_ZONE, Settings.Global.getString(cr, Settings.Global.AUTO_TIME_ZONE)); // 1 or 0
+      kvs.put(Settings.Global.BLUETOOTH_ON, Settings.Global.getString(cr, Settings.Global.BLUETOOTH_ON)); // 1 or 0
+      kvs.put(Settings.Global.DATA_ROAMING, Settings.Global.getString(cr, Settings.Global.DATA_ROAMING));  // 1 or 0
+      kvs.put(Settings.Global.HTTP_PROXY, Settings.Global.getString(cr, Settings.Global.HTTP_PROXY));  // host:port
     } else {
-      kvs.set(Settings.System.AIRPLANE_MODE_ON, Settings.System.getString(cr, Settings.System.AIRPLANE_MODE_ON));
-      kvs.set(Settings.System.ALWAYS_FINISH_ACTIVITIES, Settings.System.getString(cr, Settings.System.ALWAYS_FINISH_ACTIVITIES));
-      kvs.set(Settings.System.AUTO_TIME, Settings.System.getString(cr, Settings.System.AUTO_TIME));
-      kvs.set(Settings.System.AUTO_TIME_ZONE, Settings.System.getString(cr, Settings.System.AUTO_TIME_ZONE));
-      kvs.set(Settings.Secure.BLUETOOTH_ON, Settings.Secure.getString(cr, Settings.Secure.BLUETOOTH_ON));
-      kvs.set(Settings.System.DATA_ROAMING, Settings.System.getString(cr, Settings.System.DATA_ROAMING));
-      kvs.set(Settings.Secure.HTTP_PROXY, Settings.Secure.getString(cr, Settings.Secure.HTTP_PROXY));
+      kvs.put(Settings.System.AIRPLANE_MODE_ON, Settings.System.getString(cr, Settings.System.AIRPLANE_MODE_ON));
+      kvs.put(Settings.System.ALWAYS_FINISH_ACTIVITIES, Settings.System.getString(cr, Settings.System.ALWAYS_FINISH_ACTIVITIES));
+      kvs.put(Settings.System.AUTO_TIME, Settings.System.getString(cr, Settings.System.AUTO_TIME));
+      kvs.put(Settings.System.AUTO_TIME_ZONE, Settings.System.getString(cr, Settings.System.AUTO_TIME_ZONE));
+      kvs.put(Settings.Secure.BLUETOOTH_ON, Settings.Secure.getString(cr, Settings.Secure.BLUETOOTH_ON));
+      kvs.put(Settings.Secure.DATA_ROAMING, Settings.Secure.getString(cr, Settings.Secure.DATA_ROAMING));
+      kvs.put(Settings.Secure.HTTP_PROXY, Settings.Secure.getString(cr, Settings.Secure.HTTP_PROXY));
     }
 
-    kvs.set(Settings.Secure.ACCESSIBILITY_ENABLED, Settings.Secure.getString(cr, Settings.Secure.ACCESSIBILITY_ENABLED)); // 1 or 0
-    kvs.set(Settings.Secure.INSTALL_NON_MARKET_APPS, Settings.Secure.getString(cr, Settings.Secure.INSTALL_NON_MARKET_APPS)); // 1 or 0
+    kvs.put(Settings.Secure.ACCESSIBILITY_ENABLED, Settings.Secure.getString(cr, Settings.Secure.ACCESSIBILITY_ENABLED)); // 1 or 0
+    kvs.put(Settings.Secure.INSTALL_NON_MARKET_APPS, Settings.Secure.getString(cr, Settings.Secure.INSTALL_NON_MARKET_APPS)); // 1 or 0
 
-    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN)
-      kvs.set(Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED, Settings.Secure.getString(cr, Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED)); // 1 or 0
-    else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN)
-      kvs.set(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, Settings.Global.getString(cr, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED));
+    if (Build.VERSION.SDK_INT == Build.VERSION_CODES.JELLY_BEAN) {
+      kvs.put(Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED, Settings.Secure.getString(cr, Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED)); // 1 or 0
+    }
+    else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+      kvs.put(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, Settings.Global.getString(cr, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED));
+    }
 
-    kvs.set(Settings.System.DATE_FORMAT, Settings.System.getString(cr, Settings.System.DATE_FORMAT)); // dd/mm/yyyy
-    kvs.set(Settings.System.SCREEN_OFF_TIMEOUT, Settings.System.getString(cr, Settings.System.SCREEN_OFF_TIMEOUT)); // milliseconds
-    kvs.set(Settings.System.TIME_12_24, Settings.System.getString(cr, Settings.System.TIME_12_24)); // 12 or 24
+    kvs.put(Settings.System.DATE_FORMAT, Settings.System.getString(cr, Settings.System.DATE_FORMAT)); // dd/mm/yyyy
+    kvs.put(Settings.System.SCREEN_OFF_TIMEOUT, Settings.System.getString(cr, Settings.System.SCREEN_OFF_TIMEOUT)); // milliseconds
+    kvs.put(Settings.System.TIME_12_24, Settings.System.getString(cr, Settings.System.TIME_12_24)); // 12 or 24
 
-    kvs.set(Settings.Secure.ALLOW_MOCK_LOCATION, Settings.Secure.getString(cr, Settings.Secure.ALLOW_MOCK_LOCATION)); // 1 or 0
+    kvs.put(Settings.Secure.ALLOW_MOCK_LOCATION, Settings.Secure.getString(cr, Settings.Secure.ALLOW_MOCK_LOCATION)); // 1 or 0
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-      kvs.set(Settings.Secure.LOCATION_MODE, Settings.Secure.getString(cr, Settings.Secure.LOCATION_MODE)); // Int values 0 - 3
-  }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      kvs.put(Settings.Secure.LOCATION_MODE, Settings.Secure.getString(cr, Settings.Secure.LOCATION_MODE)); // Int values 0 - 3
+    }
 
-  private void storeIfKnown(String key, String value, KeyValueStorage akv) {
-    if (!value.equals(Build.UNKNOWN))
-      akv.set(key, value);
-  }
-
-  public void getBuildParams(KeyValueStorage akv) {
-    // Most params are never changed, others are changed only after firmware upgrade.
-    akv.set("build_version_sdk", Build.VERSION.SDK_INT);
-    storeIfKnown("build_brand", Build.BRAND, akv);
+    // Most build params are never changed, others are changed only after firmware upgrade.
+    kvs.put("build_version_sdk", Build.VERSION.SDK_INT);
+    kvs.put("build_brand", Build.BRAND);
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       for (int i = 0; i < Build.SUPPORTED_ABIS.length; ++i)
-        storeIfKnown("build_cpu_abi" + (i + 1), Build.SUPPORTED_ABIS[i], akv);
+        kvs.put("build_cpu_abi" + (i + 1), Build.SUPPORTED_ABIS[i]);
     } else {
-      storeIfKnown("build_cpu_abi1", Build.CPU_ABI, akv);
-      storeIfKnown("build_cpu_abi2", Build.CPU_ABI2, akv);
+      kvs.put("build_cpu_abi1", Build.CPU_ABI);
+      kvs.put("build_cpu_abi2", Build.CPU_ABI2);
     }
-    storeIfKnown("build_device", Build.DEVICE, akv);
-    storeIfKnown("build_display", Build.DISPLAY, akv);
-    storeIfKnown("build_fingerprint", Build.FINGERPRINT, akv);
-    storeIfKnown("build_hardware", Build.HARDWARE, akv);
-    storeIfKnown("build_host", Build.HOST, akv);
-    storeIfKnown("build_id", Build.ID, akv);
-    storeIfKnown("build_manufacturer", Build.MANUFACTURER, akv);
-    storeIfKnown("build_model", Build.MODEL, akv);
-    storeIfKnown("build_product", Build.PRODUCT, akv);
-    storeIfKnown("build_serial", Build.SERIAL, akv);
-    storeIfKnown("build_tags", Build.TAGS, akv);
-    akv.set("build_time", Build.TIME);
-    storeIfKnown("build_type", Build.TYPE, akv);
-    storeIfKnown("build_user", Build.USER, akv);
+    kvs.put("build_device", Build.DEVICE);
+    kvs.put("build_display", Build.DISPLAY);
+    kvs.put("build_fingerprint", Build.FINGERPRINT);
+    kvs.put("build_hardware", Build.HARDWARE);
+    kvs.put("build_host", Build.HOST);
+    kvs.put("build_id", Build.ID);
+    kvs.put("build_manufacturer", Build.MANUFACTURER);
+    kvs.put("build_model", Build.MODEL);
+    kvs.put("build_product", Build.PRODUCT);
+    kvs.put("build_serial", Build.SERIAL);
+    kvs.put("build_tags", Build.TAGS);
+    kvs.put("build_time", Build.TIME);
+    kvs.put("build_type", Build.TYPE);
+    kvs.put("build_user", Build.USER);
+
+    Statistics.logEvent("$AndroidDeviceInfo", kvs.mPairs);
   }
+
 }
