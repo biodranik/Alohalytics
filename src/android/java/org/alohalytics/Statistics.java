9578417b49438afc2ae9e988fa1dfff104f3450e
Alex Zolotarev <alex@maps.me>
2015-02-19 20:53:11 +0300

    [android] Log install and update location if the app has permissions. @TODO: make location logging optional.

--- src/android/java/org/alohalytics/Statistics.java
+++ src/android/java/org/alohalytics/Statistics.java
@@ -72,19 +72,22 @@ public class Statistics {
       ex.printStackTrace();
     }
     final SharedPreferences prefs = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
+    // Requires ACCESS_FINE_LOCATION permission.
+    final LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    final Location lastKnownLocation = (lm == null) ? null : lm.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
     // Is it a real new install?
     if (id.second && installTime == updateTime) {
-      final LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
       logEvent("$install", new String[]{"version", versionName,
           "secondsBeforeLaunch", String.valueOf((System.currentTimeMillis() - installTime) / 1000)},
-          lm == null ? null : lm.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER));
+          lastKnownLocation);
       // Collect device info once on start.
       SystemInfo.getDeviceInfoAsync(context);
       prefs.edit().putLong(PREF_APP_UPDATE_TIME, updateTime).apply();
     } else if (updateTime != installTime && updateTime != prefs.getLong(PREF_APP_UPDATE_TIME, 0)) {
       logEvent("$update", new String[]{"version", versionName,
           "secondsBeforeLaunch", String.valueOf((System.currentTimeMillis() - updateTime) / 1000),
-          "userAgeInSeconds", String.valueOf((System.currentTimeMillis() - installTime) / 1000)});
+          "userAgeInSeconds", String.valueOf((System.currentTimeMillis() - installTime) / 1000)},
+          lastKnownLocation);
       // Also collect device info on update.
       SystemInfo.getDeviceInfoAsync(context);
       prefs.edit().putLong(PREF_APP_UPDATE_TIME, updateTime).apply();
