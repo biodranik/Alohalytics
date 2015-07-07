a15a0d26fe61ee734107dc2ff26695224438c2ad
Alex Zolotarev <alex@maps.me>
2015-07-07 16:43:23 +0300

    - FastCGI server now always receives all data from clients. - It also supports logrotate for it’s data file. - And have more detailed error logging.

--- server/fcgi_server.cc
+++ server/fcgi_server.cc
@@ -74,6 +74,7 @@ http {
 // clang-format on
 
 #include <chrono>
+#include <csignal>
 #include <cstdlib>
 #include <ctime>
 #include <exception>
@@ -86,7 +87,6 @@ http {
 #include <fcgiapp.h>
 #include <fcgio.h>
 
-#include "../src/file_manager.h"
 #include "../src/logger.h"
 
 #include "statistics_receiver.h"
@@ -94,14 +94,63 @@ http {
 using namespace std;
 
 // Can be used as a basic check on the client-side if it has connected to the right server.
-static const string kBodyTextInSuccessfulServerReply = "Mahalo";
+static const string kBodyTextForGoodServerReply = "Mahalo";
+static const string kBodyTextForBadServerReply = "Hohono";
 
-// TODO(AlexZ): Pass log file as a command line parameter and use it for logging when running as daemon.
+// We always reply to our clients that we have received everything they sent, even if it was a complete junk.
+// The difference is only in the body of the reply.
+void Reply200OKWithBody(FCGX_Stream * out, const string & body) {
+  FCGX_FPrintF(out, "Status: 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s\n", body.size(),
+               body.c_str());
+}
+
+// Global variables to correctly reopen data and log files after signals from logrotate utility.
+volatile sig_atomic_t gReceivedSIGHUP = 0;
+volatile sig_atomic_t gReceivedSIGUSR1 = 0;
+// Redirects all cout output into a file if good log_file_path was given in constructor.
+// Can always ReopenLogFile() if needed (e.g. for log rotation).
+struct CoutToFileRedirector {
+  char const * path;
+  unique_ptr<ofstream> log_file;
+  streambuf * original_cout_buf;
+
+  CoutToFileRedirector(const char * log_file_path) : path(log_file_path), original_cout_buf(cout.rdbuf()) {
+    ReopenLogFile();
+  }
+  void ReopenLogFile() {
+    if (!path) {
+      return;
+    }
+    log_file.reset(nullptr);
+    log_file.reset(new ofstream(path, ofstream::out | ofstream::app));
+    if (log_file->good()) {
+      cout.rdbuf(log_file->rdbuf());
+    } else {
+      ATLOG("ERROR: Can't open log file", path, "for writing.");
+    }
+  }
+  // Restore original cout streambuf.
+  ~CoutToFileRedirector() { cout.rdbuf(original_cout_buf); }
+};
+
 int main(int argc, char * argv[]) {
   if (argc < 2) {
-    ALOG("Usage:", argv[0], "<directory to store received data>");
+    ALOG("Usage:", argv[0], "<directory to store received data> [path to error log file]");
+    ALOG("  - SIGHUP reopens main data file and SIGUSR1 reopens debug log file for logrotate utility.");
     return -1;
   }
+
+  // Redirect cout into a file if it was given in the command line.
+  CoutToFileRedirector log_redirector(argc > 2 ? argv[2] : nullptr);
+  // Correctly reopen data file on SIGHUP for logrotate.
+  if (SIG_ERR == ::signal(SIGHUP, [](int) { gReceivedSIGHUP = SIGHUP; })) {
+    ATLOG("WARNING: Can't set SIGHUP handler. Logrotate will not work correctly.");
+  }
+  // Correctly reopen debug log file on SIGUSR1 for logrotate.
+  if (SIG_ERR == ::signal(SIGUSR1, [](int) { gReceivedSIGUSR1 = SIGUSR1; })) {
+    ATLOG("WARNING: Can't set SIGUSR1 handler. Logrotate will not work correctly.");
+  }
+
   int result = FCGX_Init();
   if (0 != result) {
     ALOG("ERROR: FCGX_Init has failed with code", result);
@@ -115,54 +164,69 @@ int main(int argc, char * argv[]) {
   }
   alohalytics::StatisticsReceiver receiver(argv[1]);
   string gzipped_body;
-  ALOG("FastCGI Server instance is ready to serve clients' requests.");
+  long long content_length;
+  const char * remote_addr_str;
+  const char * request_uri_str;
+  const char * user_agent_str;
+  ATLOG("FastCGI Server instance is ready to serve clients' requests.");
   while (FCGX_Accept_r(&request) >= 0) {
+    // Correctly reopen data file in the queue.
+    if (gReceivedSIGHUP == SIGHUP) {
+      receiver.ReopenDataFile();
+      gReceivedSIGHUP = 0;
+    }
+    // Correctly reopen debug log file.
+    if (gReceivedSIGUSR1 == SIGUSR1) {
+      log_redirector.ReopenLogFile();
+      gReceivedSIGUSR1 = 0;
+    }
+    FCGX_FPrintF(request.out, "Status: 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s\n",
+                 kBodyTextForGoodServerReply.size(), kBodyTextForGoodServerReply.c_str());
     try {
-      const char * content_length_str = FCGX_GetParam("HTTP_CONTENT_LENGTH", request.envp);
-      long long content_length;
-      if (!content_length_str || ((content_length = atoll(content_length_str)) <= 0)) {
-        ALOG("WARNING: Invalid or missing Content-Length header, request is ignored.");
-        FCGX_FPrintF(request.out,
-                     "Status: 411 Length Required\r\nContent-Type: text/plain\r\n\r\n411 Length Required\n");
-        continue;
+      remote_addr_str = FCGX_GetParam("REMOTE_ADDR", request.envp);
+      if (!remote_addr_str) {
+        ATLOG("WARNING: Missing REMOTE_ADDR. Please check your http server configuration.");
+        remote_addr_str = "";
       }
-      gzipped_body.resize(content_length);
-      if (fcgi_istream(request.in).read(&gzipped_body[0], content_length).fail()) {
-        ALOG("WARNING: Can't read body contents, request is ignored.");
-        FCGX_FPrintF(request.out, "Status: 400 Bad Request\r\nContent-Type: text/plain\r\n\r\n400 Bad Request\n");
-        continue;
+      request_uri_str = FCGX_GetParam("REQUEST_URI", request.envp);
+      if (!request_uri_str) {
+        ATLOG("WARNING: Missing REQUEST_URI. Please check your http server configuration.");
+        request_uri_str = "";
+      }
+      user_agent_str = FCGX_GetParam("HTTP_USER_AGENT", request.envp);
+      if (!user_agent_str) {
+        ATLOG("WARNING: Missing HTTP User-Agent. Please check your http server configuration.");
+        user_agent_str = "";
       }
 
-      const char * user_agent_str = FCGX_GetParam("HTTP_USER_AGENT", request.envp);
-      if (!user_agent_str) {
-        ALOG("WARNING: Missing HTTP User-Agent.");
+      const char * content_length_str = FCGX_GetParam("HTTP_CONTENT_LENGTH", request.envp);
+      content_length = 0;
+      if (!content_length_str || ((content_length = atoll(content_length_str)) <= 0)) {
+        ATLOG("WARNING: Request is ignored due to invalid or missing Content-Length header", content_length_str,
+              remote_addr_str, request_uri_str, user_agent_str);
+        Reply200OKWithBody(request.out, kBodyTextForBadServerReply);
+        continue;
       }
-      const char * request_uri_str = FCGX_GetParam("REQUEST_URI", request.envp);
-      if (!request_uri_str) {
-        ALOG("WARNING: Missing REQUEST_URI.");
-      }
-      const char * remote_addr_str = FCGX_GetParam("REMOTE_ADDR", request.envp);
-      if (!remote_addr_str) {
-        ALOG("WARNING: Missing REMOTE_ADDR.");
+      // TODO(AlexZ): Should we make a better check for Content-Length or basic exception handling would be enough?
+      gzipped_body.resize(content_length);
+      if (fcgi_istream(request.in).read(&gzipped_body[0], content_length).fail()) {
+        ATLOG("WARNING: Request is ignored because it's body can't be read.", remote_addr_str, request_uri_str,
+              user_agent_str);
+        Reply200OKWithBody(request.out, kBodyTextForBadServerReply);
+        continue;
       }
 
       // Process and store received body.
       // This call can throw different exceptions.
-      receiver.ProcessReceivedHTTPBody(gzipped_body, AlohalyticsBaseEvent::CurrentTimestamp(),
-                                       remote_addr_str ? remote_addr_str : "", user_agent_str ? user_agent_str : "",
-                                       request_uri_str ? request_uri_str : "");
-      FCGX_FPrintF(request.out, "Status: 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %ld\r\n\r\n%s\n",
-                   kBodyTextInSuccessfulServerReply.size(), kBodyTextInSuccessfulServerReply.c_str());
-
+      receiver.ProcessReceivedHTTPBody(gzipped_body, AlohalyticsBaseEvent::CurrentTimestamp(), remote_addr_str,
+                                       user_agent_str, request_uri_str);
+      Reply200OKWithBody(request.out, kBodyTextForGoodServerReply);
     } catch (const exception & ex) {
-      ALOG("ERROR: Exception while processing request: ", ex.what());
-      FCGX_FPrintF(request.out,
-                   "Status: 500 Internal Server Error\r\nContent-Type: text/plain\r\n\r\n500 Internal Server Error\n");
-      // TODO(AlexZ): Full log, possibly into the file, to get more details about the error. Also think about clients
-      // who can constantly fail because of bad data file.
-      continue;
+      ATLOG("ERROR: Exception was thrown:", ex.what(), remote_addr_str, request_uri_str, user_agent_str);
+      // TODO(AlexZ): Log "bad" received body for investigation.
+      Reply200OKWithBody(request.out, kBodyTextForBadServerReply);
     }
   }
-  ALOG("Shutting down FastCGI server instance.");
+  ATLOG("Shutting down FastCGI server instance.");
   return 0;
 }
